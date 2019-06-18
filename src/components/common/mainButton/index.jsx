b90d6c9621e76f58429c43e8020960064b23fd3b
Isaac <garciaisaac468@gmail.com>
2019-06-18 10:18:19 -0500

    customize main button

---
+++ src/components/common/mainButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const MainButton = ( props ) =>(
+    <button className="main-button">
+        botón primario
+    </button>
+)
+
+export default MainButton

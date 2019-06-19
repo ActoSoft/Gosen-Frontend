b59fde5183042576051f3a340acdb5bf95952248
Isaac <garciaisaac468@gmail.com>
2019-06-19 16:12:33 -0500

    tipe input pass in props

--- src/components/common/inputText/index.jsx
+++ src/components/common/inputText/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.scss'
 
 const InputText = ( props ) =>(
-    <input type={"text"} className="input-text-component"/>
+    <input type={ props.type ? props.type : null } className="input-text-component"/>
 )
 
 export default InputText

6996b45bc102ec96d8ebd7f808b6c6e7d9a9e373
Isaac <garciaisaac468@gmail.com>
2019-06-22 21:12:36 -0500

    start ui of login

--- src/components/authentication/login/index.jsx
+++ src/components/authentication/login/index.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './index.scss'
+import GLogo from '../../../assets/icons/gosen_logo.png'
+import GName  from '../../../assets/icons/gosen_name.png'
 
-const Login = ( props ) =>(
-    <p>Login</p>
-)
+class Login extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div className="login-ui-container">
+                <div className="left-container">
+                    <span>Iniciar sesión</span>
+                    <div className="form-part">
+                        <span>Usuario</span>
+                        <input type="text"/>
+                        <span>Contraseña</span>
+                        <input type="password"/>
+                        <p>¿Olvidaste tu contraseña?</p>
+                    </div>
+                    <button>Contraseña</button>
+                </div>
+                <div className="right-container">
+                    <img src={ GLogo } alt="Logo Gosen" />
+                    <img src={ GName } alt="Nombre Gosen" />
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Login

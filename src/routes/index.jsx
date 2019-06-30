cd61f9ad960d9fa8099bf7396029ef9fa295e16b
Isaac <garciaisaac468@gmail.com>
2019-06-30 13:06:41 -0500

    add confirm password component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Login from '../components/authentication/login'
 import ForgotPassword from '../components/authentication/forgotPassword'
+import ConfirmPassword from '../components/authentication/confirmPassword'
 import Dashboard from '../components/dashboard'
 import PrivateRoute from './privateRoute'
 
@@ -14,6 +15,7 @@ const Routes = () => {
                 {/* Public Routes */}
                 <Route exact path = "/login/" component = { Login } />
                 <Route exact path = "/recuperar-contraseña/" component = { ForgotPassword } />
+                <Route exact path = "/confirmar-contraseña/" component = { ConfirmPassword }/>
 
                 {/* Private Routes */}
                 <PrivateRoute exact path = '/dashboard/' component = { Dashboard } />
